13e0037169f2e81d535d98ed6d23bc023486b8f5
Numidia228 <you@example.com>
2020-09-13 11:09:28 +0300

    Заготовка проекта

---
+++ src/com/company/Work3.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Work3 {
+
+    public static void main(String[] args) {
+        double celsium = 45;
+        double fahrenheit = 9 / 5.0 * celsium + 32;
+        System.out.println("Температура в Фаренгейтах: " + Math.round(fahrenheit));
+    }
+}
